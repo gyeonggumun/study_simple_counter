f32af73c7704692ea8e83721272e9d0a96cdb52b
gyeonggumun <lion989072@gmail.com>
2026-02-22 21:41:53 +0900

    feat: 버튼 상태적용 로직 추가

--- src/component/Controller.js
+++ src/component/Controller.js
@@ -1,12 +1,19 @@
+import { useState } from "react";
+
 const Controller = () => {
+    const [count, setCount] = useState(0);
+    const handleSetCount = (value) => {
+        setCount(count + value);
+    };
+
     return (
         <div>
-            <button>+1</button>
-            <button>+10</button>
-            <button>+100</button>
-            <button>-100</button>
-            <button>-10</button>
-            <button>-1</button>
+            <button onClick={()=> handleSetCount(-1)}>-1</button>
+            <button onClick={()=> handleSetCount(-10)}>-10</button>
+            <button onClick={()=> handleSetCount(-100)}>-100</button>
+            <button onClick={()=> handleSetCount(100)}>+100</button>
+            <button onClick={()=> handleSetCount(10)}>+10</button>
+            <button onClick={()=> handleSetCount(1)}>+1</button>
         </div>
     );
 };
