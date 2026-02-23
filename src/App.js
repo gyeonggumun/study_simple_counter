28842b67bc4aa0ad86ebeff266aa3d2ad263fe97
gyeonggumun <lion989072@gmail.com>
2026-02-23 20:57:34 +0900

    feat: count값이 변환할 때 console에 출력하도록 변경

--- src/App.js
+++ src/App.js
@@ -1,14 +1,19 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import './App.css';
 import Controller from './component/Controller';
 import Viewer from './component/Viewer';
 
 function App() {
   const [count, setCount] = useState(0);
+
   const handleSetCount = (value) => {
     setCount(count + value);
   };
 
+  useEffect(() => {
+    console.log("count 업데이트: ", count);
+  }, [count]);
+  
   return <div className="App">
     <h1>Simple Counter</h1>
     <section>
